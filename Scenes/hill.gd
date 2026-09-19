83f69ba4eb39f998109431a3115fa0f0be83b696
Eeby Sheeb <eebysheebstudio@gmail.com>
2026-09-19 08:19:46 -0700

    Add Progress Bar

--- Scenes/hill.gd
+++ Scenes/hill.gd
@@ -1,15 +1,24 @@
 extends Node2D
-var screen_speed: float = 0.1
+var hill_bottom: float = 0.0
+var hill_top = 1000.0
+var percent_progress_speed: float = 5 # Moves X% of the hill per second 
+var percent_progress: float = 0.0
+var hill_height: float
+var players: Array[Player]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	hill_height = hill_top - hill_bottom
+	$MovementNode.position = Vector2(0, hill_bottom)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$MovementNode.position = $MovementNode.position + Vector2(0, -screen_speed)
-
+	if (percent_progress < 100):
+		percent_progress = percent_progress + (percent_progress_speed * delta)
+		$MovementNode.position = $MovementNode.position + Vector2(0, -hill_height * percent_progress / 100)
+		$CanvasLayer/MarginContainer/HillProgressBar.update_progress(percent_progress)
 
 func _on_kill_box_body_entered(body: Node2D) -> void:
 	if body is Player:

f8b1d920339022fc3f7b7441d713f19b1031fcf0
Eeby Sheeb <eebysheebstudio@gmail.com>
2026-09-18 21:15:53 -0700

    Add Essential Infrastructure

---
+++ Scenes/Obstacles/obstacle_interface.gd
@@ -0,0 +1,20 @@
+extends Node2D
+class_name ObstacleInterface
+
+@export var area: Area2D = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	area.body_entered.connect(_on_area_2d_body_entered)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Player:
+		obstacle_effect(body)
+		
+func obstacle_effect(player: Player):
+	print("YOU TRIGGERED THE OBSTACLE INTERFACE EFFECT")

35d4aa9f305d541d0d8860778d4b8bd27237b246
Eeby Sheeb <eebysheebstudio@gmail.com>
2026-09-20 00:16:54 -0700

    Update Main Menu

--- Scenes/UI/main_menu.gd
+++ Scenes/UI/main_menu.gd
@@ -12,7 +12,7 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func get_focus() -> void:
-	$MarginContainer/VBoxContainer/Start.grab_focus()
+	$TextureRect/MarginContainer/VBoxContainer/Start.grab_focus()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -22,49 +22,49 @@ func _input(event: InputEvent) -> void:
 	#don't judge me
 	if event.is_action_pressed("p1_start"):
 		players[0] = true
-		$HBoxContainer/ColorRect.set_color(Color(0,1,0,1))
+		$TextureRect/MarginContainer/VBoxContainer/PlayerIndication/ColorRect.set_color(Color(0.219, 0.467, 0.172, 1.0))
 		player_enter.emit()
 	elif event.is_action_pressed("p2_start"):
 		players[1] = true
-		$HBoxContainer/ColorRect2.set_color(Color(0,1,0,1))
+		$TextureRect/MarginContainer/VBoxContainer/PlayerIndication/ColorRect2.set_color(Color(0.219, 0.467, 0.172, 1.0))
 		player_enter.emit()
 	elif event.is_action_pressed("p3_start"):
 		players[2] = true
-		$HBoxContainer/ColorRect3.set_color(Color(0,1,0,1))
+		$TextureRect/MarginContainer/VBoxContainer/PlayerIndication/ColorRect3.set_color(Color(0.219, 0.467, 0.172, 1.0))
 		player_enter.emit()
 	elif event.is_action_pressed("p4_start"):
 		players[2] = true
-		$HBoxContainer/ColorRect4.set_color(Color(0,1,0,1))
+		$TextureRect/MarginContainer/VBoxContainer/PlayerIndication/ColorRect4.set_color(Color(0.219, 0.467, 0.172, 1.0))
 		player_enter.emit()
 
 	if event.is_action_pressed("p1_back") && players[0]:
 		players[0] = false
-		$HBoxContainer/ColorRect.set_color(Color(1,0,0,1))
+		$TextureRect/MarginContainer/VBoxContainer/PlayerIndication/ColorRect.set_color(Color(0.663, 0.008, 0.0, 1.0))
 		player_exit.emit()
 	elif event.is_action_pressed("p2_back") && players[1]:
 		players[1] = false
-		$HBoxContainer/ColorRect2.set_color(Color(1,0,0,1))
+		$TextureRect/MarginContainer/VBoxContainer/PlayerIndication/ColorRect2.set_color(Color(0.663, 0.008, 0.0, 1.0))
 		player_exit.emit()
 	elif event.is_action_pressed("p3_back") && players[2]:
 		players[2] = false
-		$HBoxContainer/ColorRect3.set_color(Color(1,0,0,1))
+		$TextureRect/MarginContainer/VBoxContainer/PlayerIndication/ColorRect3.set_color(Color(0.663, 0.008, 0.0, 1.0))
 		player_exit.emit()
 	elif event.is_action_pressed("p4_back") && players[3]:
 		players[3] = false
-		$HBoxContainer/ColorRect3.set_color(Color(1,0,0,1))
+		$TextureRect/MarginContainer/VBoxContainer/PlayerIndication/ColorRect4.set_color(Color("a90200ff"))
 		player_exit.emit()
 		
 	if (event.is_action_pressed("p1_action") 
-	&& $MarginContainer/VBoxContainer/Start.has_focus()
+	&& $TextureRect/MarginContainer/VBoxContainer/Start.has_focus()
 	&& players[0] == true):
 		_on_start_pressed()
 	
 	if (event.is_action_pressed("p1_action") 
-	&& $MarginContainer/VBoxContainer/Settings.has_focus()):
+	&& $TextureRect/MarginContainer/VBoxContainer/Settings.has_focus()):
 		_on_settings_pressed()
 	
 	if (event.is_action_pressed("p1_action") 
-	&& $MarginContainer/VBoxContainer/Quit.has_focus()):
+	&& $TextureRect/MarginContainer/VBoxContainer/Quit.has_focus()):
 		_on_quit_pressed()
 		
 func _on_quit_pressed() -> void:

4e11dd31c508289bf70fdb04dee0f9e359e1fa11
zwiebelkaramell <143242173+zwiebelkaramell@users.noreply.github.com>
2026-09-20 16:12:30 -0600

    volume mixing

--- Global_Settings.gd
+++ Global_Settings.gd
@@ -6,9 +6,9 @@ static var pup_speed_dur = 1.0
 static var pup_hook_dur = 1.0
 static var pup_cannon_dur = 1.0
 
-@onready var master_vol = 1.0
-@onready var sfx_vol = 0.5
-@onready var music_vol = 0.5
+var master_vol = 1.0
+var sfx_vol = 0.6
+var music_vol = 0.2
 
 
 

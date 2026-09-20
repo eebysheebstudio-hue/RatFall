dac9cd9fe05419baad5412a70e3e23a30f165d00
zwiebelkaramell <143242173+zwiebelkaramell@users.noreply.github.com>
2026-09-20 14:50:02 -0600

    fixing pernicious bugs

--- Scenes/UI/settings_menu.gd
+++ Scenes/UI/settings_menu.gd
@@ -27,7 +27,7 @@ func _on_master_value_changed(value: float) -> void:
 	volume_changed.emit()
 
 func _input(event: InputEvent) -> void:
-	if (event.is_action_pressed("p1_back")):
+	if (event.is_action_pressed("p1_back") && self.is_visible()):
 		UiSwitcher.show_main_menu()
 
 func _on_button_button_up() -> void:
